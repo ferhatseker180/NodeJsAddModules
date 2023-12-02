b21b629708c4ad1713fd77a984c4a9c36a0abc00
ferhatseker180 <ferhatseker180@gmail.com>
2023-12-02 21:58:00 +0300

    NodeJsAddModules
    We learn and made NPM and External Node Modules

---
+++ index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+
+import generateName from "sillyname";
+
+import superHeroGenerateName from "superheroes";
+
+var sillyName = generateName();
+const superHeroName = superHeroGenerateName.random();
+
+// console.log(sillyName);
+console.log(superHeroName);
